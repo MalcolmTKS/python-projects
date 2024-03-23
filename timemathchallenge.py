6b7b1322c9cc49987abc842957dc898ca82fa792
0xM <109440437+MalcolmTKS@users.noreply.github.com>
2024-03-23 14:34:03 -0400

    Update timemathchallenge.py

--- timemathchallenge.py
+++ timemathchallenge.py
@@ -1,11 +1,27 @@
-#Generate random math questions, time based, and not allow to continuation until answered. 
+#Generate random math questions, time-based, and do not allow continuation until answered correctly. 
 
 import random 
 
 OPERATORS = ["+", "-", "*"]
 MIN_OPERAND = 3
 MAX_OPERAND = 12
+TOTAL_PROBLEMS = 10
 
 def generate_problem():
     left = random.randint(MIN_OPERAND, MAX_OPERAND)
     right = random.randint(MIN_OPERAND, MAX_OPERAND)
+    operator = random.choice(OPERATORS)
+    
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer
+    
+
+
+for i in range(TOTAL_PROBLEMS):
+    expr, answer = generate_problem()
+    while True:
+       guess = input("Problem #" + str(i + 1) + ": " + expr + " = ")
+       if guess == str(answer):
+           break
+    
