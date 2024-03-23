9e2a329213a968089a2dcb8a6e8343a92cd25c0b
0xM <109440437+MalcolmTKS@users.noreply.github.com>
2024-03-23 00:16:26 -0400

    Add files via upload

---
+++ timemathchallenge.py
@@ -0,0 +1,11 @@
+#Generate random math questions, time based, and not allow to continuation until answered. 
+
+import random 
+
+OPERATORS = ["+", "-", "*"]
+MIN_OPERAND = 3
+MAX_OPERAND = 12
+
+def generate_problem():
+    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    right = random.randint(MIN_OPERAND, MAX_OPERAND)
