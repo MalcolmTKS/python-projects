093b594f723b9942c1e4986bafcba5bf9f919f36
0xM <109440437+MalcolmTKS@users.noreply.github.com>
2024-04-07 18:16:12 -0400

    Add files via upload

---
+++ gamble.py
@@ -0,0 +1,68 @@
+
+
+MAX_LINES = 3 
+MAX_BET = 100
+MIN_BET = 1
+
+#Collecting user deposit. 
+def deposit():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+          amount = int(amount)
+          if amount > 0:
+              break 
+          else:
+              print("Amount must be greater than 0.")
+        else: 
+            print("Please enter a number.")
+            
+    return amount
+
+def get_number_of_lines():
+  while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")? ")
+        if lines.isdigit():
+          lines = int(lines)
+          if 1 <= lines <= MAX_LINES:
+              break 
+          else:
+              print("Enter a valid number of lines")
+        else: 
+            print("Please enter a number.")
+            
+  return lines
+
+def get_bet():
+   while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+          amount = int(amount)
+          if MIN_BET <= amount <= MAX_BET:
+              break
+          if amount > 0:
+              break 
+          else:
+              print(f"Amount must be greater than ${MIN_BET} - ${MAX_BET}.")
+        else: 
+            print("Please enter a number.")
+            
+   return amount
+
+def main():
+    balance = deposit()
+    lines = get_number_of_lines()
+    while True:
+        bet = get_bet()
+        total_bet = bet * lines 
+        
+        if total_bet > balance: 
+            print(
+                f"you do not have enough to bet that amount, your current balance is: ${balance}")
+        else:
+            break
+        
+        
+    print(f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
+ 
+main()
